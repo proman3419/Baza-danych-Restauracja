330410d8c1d15219c5c4a34bd144a1a3ec6ce8bc
Wojciech Przybytek <80359324+wprzybytek@users.noreply.github.com>
2022-01-19 20:17:40 +0100

    Create generateDiscountHist.sql

---
+++ helpful_queries/generateDiscountHist.sql
@@ -0,0 +1,17 @@
+insert into SingleUseDiscountsHist (K2, R2, D1, StartDate, EndDate)
+Values (1000, 0.05, 7, '01/01/2020 00:0 AM', '01/01/2021 00:0 AM')
+
+insert into SingleUseDiscountsHist (K2, R2, D1, StartDate, EndDate)
+Values (1200, 0.06, 8, '01/01/2021 00:0 AM', '01/01/2022 00:0 AM')
+
+insert into SingleUseDiscountsHist (K2, R2, D1, StartDate, EndDate)
+Values (1400, 0.07, 9, '01/01/2022 00:0 AM', null)
+
+insert into LifeLongDiscountsHist (Z1, K1, R1, StartDate, EndDate)
+Values (10, 30, 0.03, '01/01/2020 00:0 AM', '01/01/2021 00:0 AM')
+
+insert into LifeLongDiscountsHist (Z1, K1, R1, StartDate, EndDate)
+Values (11, 25, 0.04, '01/01/2021 00:0 AM', '01/01/2022 00:0 AM')
+
+insert into LifeLongDiscountsHist (Z1, K1, R1, StartDate, EndDate)
+Values (12, 20, 0.05, '01/01/2022 00:0 AM', null)
